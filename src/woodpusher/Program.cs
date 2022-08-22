b13cd1dd5cf735e72a0e6427befc4ed42ce31733
Mikael Fredriksson <frimurare@gmail.com>
2022-08-22 08:45:09 +0200

    setting up

---
+++ src/woodpusher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace net.sictransit.woodpusher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
