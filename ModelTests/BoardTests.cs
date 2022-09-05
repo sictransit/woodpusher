6dad93fa4c0c9eac319ade502fa745d8df750dc9
Mikael Fredriksson <micke@sictransit.net>
2022-09-05 11:22:03 +0200

    test for Play() is green

--- ModelTests/BoardTests.cs
+++ ModelTests/BoardTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SicTransit.Woodpusher.Model;
 using SicTransit.Woodpusher.Model.Enums;
+using SicTransit.Woodpusher.Model.Extensions;
+using System.Diagnostics;
 
 namespace SicTransit.Woodpusher.Tests
 {
@@ -126,6 +128,56 @@ namespace SicTransit.Woodpusher.Tests
             }
 
             Assert.AreEqual(0, board.GetPositionsOnFile(PieceColour.White, file).Count());
+        }
+
+        [TestMethod]
+        public void PlayTest()
+        {
+            var whiteBishop = new Piece(PieceType.Bishop, PieceColour.White);
+            var blackPawn = new Piece(PieceType.Pawn, PieceColour.Black);
+
+            var c1 = Square.FromAlgebraicNotation("c1");
+            var g7 = Square.FromAlgebraicNotation("g7");
+
+            var board = new Board().AddPiece(c1, whiteBishop).AddPiece(g7, blackPawn);
+
+            Assert.AreEqual(0, board.Counters.HalfmoveClock);
+            Assert.AreEqual(0, board.Counters.FullmoveNumber);
+            Assert.AreEqual(PieceColour.White, board.Counters.ActiveColour);
+            Assert.IsNull(board.Counters.EnPassantTarget);
+
+            Trace.WriteLine(board.PrettyPrint());
+
+            var d2 = Square.FromAlgebraicNotation("d2");
+
+            board = board.Play(new (new (whiteBishop, c1), new (d2)));
+            Assert.AreEqual(PieceColour.Black, board.Counters.ActiveColour);
+            Assert.AreEqual(1, board.Counters.HalfmoveClock);
+            Assert.AreEqual(0, board.Counters.FullmoveNumber);
+
+            Trace.WriteLine(board.PrettyPrint());
+
+            var g5 = Square.FromAlgebraicNotation("g5");
+
+            board = board.Play(new(new(blackPawn, g7), new(g5, SpecialMove.CannotTake, Square.FromAlgebraicNotation("g6"))));
+            Assert.AreEqual(PieceColour.White, board.Counters.ActiveColour);
+            Assert.AreEqual(Square.FromAlgebraicNotation("g6"), board.Counters.EnPassantTarget);
+            Assert.AreEqual(0, board.Counters.HalfmoveClock);
+            Assert.AreEqual(1, board.Counters.FullmoveNumber);
+
+            Trace.WriteLine(board.PrettyPrint());
+
+            board = board.Play(new(new(whiteBishop, d2), new(g5)));
+            Assert.IsNull(board.Counters.EnPassantTarget);
+            Assert.AreEqual(0, board.Counters.HalfmoveClock);
+            Assert.AreEqual(1, board.Counters.FullmoveNumber);
+
+            Assert.AreEqual(0, board.GetPositions(PieceColour.Black).Count());
+            Assert.AreEqual(1, board.GetPositions(PieceColour.White).Count());
+
+            Assert.AreEqual(whiteBishop, board.Get(g5));
+
+            Trace.WriteLine(board.PrettyPrint());
         }
     }
 }
