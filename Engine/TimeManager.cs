4f25c71cc6695503c6736093bd9f3d4da6a37b3c
Mikael Fredriksson <micke@sictransit.net>
2022-11-18 17:21:25 +0100

    implementing a time manager

--- Engine/TimeManager.cs
+++ Engine/TimeManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace SicTransit.Woodpusher.Engine
 {
     internal class TimeManager
     {
+
+        // y = 0.577e3.2472x
+
+        private int timeLimit;
+        private readonly Stopwatch stopwatch = new();
+        private readonly Dictionary<int, List<int>> searches = new();
+
+        public TimeManager()
+        {
+            
+        }
+
+        public void Reset(int timeLimit)
+        {
+            this.timeLimit = timeLimit;
+            stopwatch.Restart();
+        }
+
+        public void AddNode(Node node)
+        {
+
+        }
+
     }
 }
